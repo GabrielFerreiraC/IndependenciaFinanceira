831aa1d8ec4cc2a8b05ea2e2e1828881b29ca33c
Gabriel Ferreira <gfdc2204@gmail.com>
2022-09-23 19:37:10 -0300

    adiciona a solução do exercício

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿decimal gastosMensais, valorAInvestir;
+double dividendo;
+
+
+Console.WriteLine("---Vivendo de Dividendos---\n");
+
+
+Console.WriteLine("Vivendo de Dividendos ---\n");
+
+
+Console.WriteLine("Gastos Mensais (em R$)...: ");
+gastosMensais = Convert.ToDecimal (Console.ReadLine());
+
+Console.WriteLine(" Rendimentos Mensais (%)...:");
+dividendo = Convert.ToDouble(Console.ReadLine());
+
+valorAInvestir = gastosMensais / Convert.ToDecimal(dividendo/100);
+
+
+Console.WriteLine($"\nPara receber {gastosMensais:C} por mês com rendimentos {dividendo:N1}% você precisa ter investido {valorAInvestir:C}.");
